48cf44beadd4d45397d6d8b82df02d7f9093f393
Nova <zakary.paine@gmail.com>
2023-09-16 21:21:47 -0600

    - Show timestamp before processing. - New step in the process. Removes all admin roles from the user. - Adjust method function signatures to represent a clearer abstraction. - Check if user sign-in is already blocked before blocking. - Check if mailbox is already hidden from the GAL. - Check if user has membership to corporate group before trying to remove. - Check if mailbox is already being forwarded to the intended address. - Show more info when there is an issue forwarding to the intended address. - Polish and refactoring.

--- Deactivate-M365User.ps1
+++ Deactivate-M365User.ps1
@@ -1,4 +1,4 @@
-# Version 1.3
+# Version 1.4
 
 # functions
 function Initialize-ColorScheme
@@ -19,6 +19,7 @@ function Show-Introduction
                 "- Converts their email to a shared mailbox. `n" +
                 "- Removes all their licenses. `n" +
                 "- Removes their membership to the `"Blue Raven Corporate`" group. `n" +
+                "- Removes all admin roles. `n" +
                 "- Checks for litigation hold, and if so, assigns the Exchange Online Plan 2 license. `n" +
                 "- If needed, forwards their emails to the mailbox of your choice. `n"
     ) -ForegroundColor $infoColor
@@ -198,11 +199,17 @@ function Prompt-YesOrNo($question)
     return $false
 }
 
-function SignOut-AllSessions($objectId)
+function Show-TimeStamp
+{
+    $timeStamp = Get-Date -Format yyyy-MM-dd-hh-mmtt
+    Write-Host $timestamp -ForegroundColor $infoColor
+}
+
+function SignOut-AllSessions($azureUser)
 {
     try
     {
-        Revoke-AzureADUserAllRefreshToken -ObjectId $objectId
+        $azureUser | Revoke-AzureADUserAllRefreshToken
         Write-Host "User signed out of all sessions." -ForegroundColor $successColor
     }
     catch
@@ -214,11 +221,17 @@ function SignOut-AllSessions($objectId)
     } 
 }
 
-function Block-SignIn($objectId)
+function Block-SignIn($azureUser)
 {
+    if (-not($azureUser.AccountEnabled))
+    {
+        Write-Host "User sign-in is already blocked." -ForegroundColor $successColor
+        return
+    }
+
     try
     {
-        Set-AzureAdUser -ObjectId $objectId -AccountEnabled $false
+        $azureUser | Set-AzureAdUser -AccountEnabled $false
         Write-Host "User sign-in blocked." -ForegroundColor $successColor
     }
     catch
@@ -229,25 +242,25 @@ function Block-SignIn($objectId)
         return
     }
 
-    $user = Get-AzureAdUser -ObjectId $objectId
-    if ($user.AccountEnabled -eq $true)
+    $updatedUser = Get-AzureAdUser -ObjectId $azureUser.ObjectId
+    if ($updatedUser.AccountEnabled)
     {
         Write-Warning "There was an issue blocking sign-in. This will need to be done manually."
     }
 }
 
-function Change-Password($objectId)
+function Set-RandomPassword($azureUser)
 {
     try
     {
         $randomPassword = [System.Web.Security.Membership]::GeneratePassword(20, 5)
         $passwordAsSecureString = ConvertTo-SecureString -String $randomPassword -AsPlainText -Force
-        Set-AzureADUserPassword -ObjectId $objectId -Password $passwordAsSecureString
-        Write-Host "User password changed." -ForegroundColor $successColor
+        $azureUser | Set-AzureADUserPassword -Password $passwordAsSecureString
+        Write-Host "User password set to something random." -ForegroundColor $successColor
     }
     catch
     {
-        Write-Host "An error occurred when changing password:" -ForegroundColor $warningColor
+        Write-Host "An error occurred when setting random password:" -ForegroundColor $warningColor
         Write-Host $_.Exception.Message -ForegroundColor $warningColor # $_ represents the ErrorRecord object
         Write-Host "This will need to be done manually." -ForegroundColor $warningColor
         return
@@ -256,6 +269,13 @@ function Change-Password($objectId)
 
 function HideFrom-GlobalAddressList($upn)
 {
+    $mailbox = Get-Mailbox -Identity $upn
+    if ($mailbox.HiddenFromAddressListsEnabled)
+    {
+        Write-Host "Mailbox is already hidden from the GAL." -ForegroundColor $successColor
+        return
+    }
+    
     try
     {
         Set-Mailbox -Identity $upn -HiddenFromAddressListsEnabled $true
@@ -270,7 +290,7 @@ function HideFrom-GlobalAddressList($upn)
     }
 
     $mailbox = Get-Mailbox -Identity $upn
-    if ($mailbox.HiddenFromAddressListsEnabled -eq $false)
+    if (-not($mailbox.HiddenFromAddressListsEnabled))
     {
         Write-Warning "There was an issue hiding the user from the GAL. This will need to be done manually."
     }
@@ -281,7 +301,7 @@ function ConvertTo-SharedMailbox($upn)
     $mailbox = Get-Mailbox -Identity $upn
     if ($mailbox.RecipientTypeDetails -eq "SharedMailbox")
     {
-        Write-Host "Mailbox is already of type: Shared" -ForegroundColor $successColor
+        Write-Host "Mailbox is already a 'Shared Mailbox'." -ForegroundColor $successColor
         return
     }
     
@@ -298,15 +318,15 @@ function ConvertTo-SharedMailbox($upn)
         return
     }
 
-<#
-Unfortunately, we cannot keep this self checking code because it happens too soon before the changes can propagate, causing false flags.
+    <#
+    Unfortunately, we cannot keep this self checking code because it happens too soon before the changes can propagate, causing false flags.
 
-$mailbox = Get-Mailbox -Identity $upn
-if ($mailbox.RecipientTypeDetails -ne "SharedMailbox")
-{
-    Write-Warning "There was an issue converting to shared mailbox. This will need to be done manually."
-}
-#>
+    $mailbox = Get-Mailbox -Identity $upn
+    if ($mailbox.RecipientTypeDetails -ne "SharedMailbox")
+    {
+        Write-Warning "There was an issue converting to shared mailbox. This will need to be done manually."
+    }
+    #>
 }
 
 function Remove-AllLicenses($azureUser)
@@ -328,7 +348,7 @@ function Remove-AllLicenses($azureUser)
         $licenses = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses
         $licenses.RemoveLicenses = $skuIds
 
-        Set-AzureADUserLicense -ObjectId $azureUser.ObjectId -AssignedLicenses $licenses
+        $azureUser | Set-AzureADUserLicense -AssignedLicenses $licenses
         Write-Host "Removed all licenses." -ForegroundColor $successColor
     }
     catch
@@ -346,11 +366,18 @@ function Remove-AllLicenses($azureUser)
     }
 }
 
-function Remove-GroupMembership($userUpn, $groupUpn)
+function Remove-GroupMembership($azureUser, $groupUpn)
 {
+    $isGroupMember = Test-IsMemberOfGroup -AzureUser $azureUser -GroupUpn $groupUpn
+    if (-not($isGroupMember))
+    {
+        Write-Host "User is not a member of $groupUpn." -ForegroundColor $successColor
+        return
+    }
+    
     try
     {
-        Remove-UnifiedGroupLinks -Links $userUpn -Identity $groupUpn -LinkType "Members" -Confirm:$false
+        Remove-UnifiedGroupLinks -Links $azureUser.UserPrincipalName -Identity $groupUpn -LinkType "Members" -Confirm:$false
         Write-Host "Removed group membership to $groupUpn." -ForegroundColor $successColor
     }
     catch
@@ -361,22 +388,69 @@ function Remove-GroupMembership($userUpn, $groupUpn)
         return
     }
 
-<#
-Unfortunately, we cannot keep this self checking code because it happens too soon before the changes can propagate, causing false flags.
+    <#
+    Unfortunately, we cannot keep this self checking code because it happens too soon before the changes can propagate, causing false flags.
+    $isGroupMember = Test-IsMemberOfGroup -AzureUser $azureUser -GroupUpn $groupUpn
+    if ($isGroupMember)
+    {
+        Write-Warning "There was an issue removing the user from group: $groupUpn. This will need to be done manually."
+    }
+    #>
+}
 
-$isStillMember = Test-IsMemberOfGroup -UserUpn $userUpn -GroupUpn $groupUpn
-if ($isStillMember)
+function Test-IsMemberOfGroup($azureUser, $groupUpn)
 {
-    Write-Warning "There was an issue removing membership to $groupUpn. This will need to be done manually."
-}
-#>
+    $userMemberships = $azureUser | Get-AzureADUserMembership
+    foreach ($group in $userMemberships)
+    {
+        if ($group.Mail -ieq $groupUpn)
+        {
+            return $true
+        }
+    }
+    return $false
 }
 
-# This function is currently unused.
-# function Test-IsMemberOfGroup($userUpn, $groupUpn)
-# {
-#     return ($null -ne (Get-AzureADGroup -SearchString $groupUpn | Get-AzureADGroupMember -All $true | Where-Object {$_.UserPrincipalName -ieq $userUpn}))
-# }
+function Remove-AllAdminRoles($azureUser)
+{
+    $allRoles = Get-AzureADDirectoryRole
+    $removedRoles = New-Object "System.Collections.Generic.List[object]"
+    $success = $true
+    foreach ($role in $allRoles)
+    {
+        try
+        {
+            if (Test-HasAdminRole -Role $role -AzureUser $azureUser)
+            {
+                Remove-AzureADDirectoryRoleMember -ObjectId $role.ObjectId -MemberId $azureUser.ObjectId
+                $removedRoles.Add($role)
+            } 
+        }
+        catch
+        {
+            $success = $false
+            Write-Host "An error occurred when removing role:" -ForegroundColor $warningColor
+            $role | Out-Host
+            Write-Host $_.Exception.Message -ForegroundColor $warningColor # $_ represents the ErrorRecord object
+            Write-Host "This will need to be done manually." -ForegroundColor $warningColor
+        }        
+    }
+
+    if ($removedRoles.Count -gt 0)
+    {
+        Write-Host "Removed the following admin roles:" -ForegroundColor $successColor
+        $removedRoles | Select-Object DisplayName, Description | Out-Host
+    }
+    elseif ($success)
+    {
+        Write-Host "User has no admin roles to remove." -ForegroundColor $successColor
+    }
+}
+
+function Test-HasAdminRole($role, $azureUser)
+{
+    return $null -ne ($role | Get-AzureADDirectoryRoleMember | Where-Object { $_.ObjectId -eq $azureUser.ObjectId })
+}
 
 function Handle-LitigationHold($upn)
 {
@@ -468,7 +542,7 @@ function Prompt-ForwardEmails($upn)
 {
     $shouldForward = Prompt-YesOrNo "Do their emails need to be forwarded?"
     if (-not($shouldForward)) { return }
-
+    
     do
     {
         $forwardingAddress = Read-Host "Enter the email address to forward to"
@@ -478,11 +552,20 @@ function Prompt-ForwardEmails($upn)
         {
             Write-Warning "Mailbox not found. Please try again."
             $foundForwardingMb = $false
+            continue
         }
         else
         {
             $foundForwardingMb = $true
-        }        
+        }
+
+        $mailbox = Get-Mailbox -Identity $upn
+        $intendedForwardingName = Get-NameFromEmail $forwardingMailbox.UserPrincipalName
+        if ($mailbox.ForwardingAddress -ieq $intendedForwardingName)
+        {
+            Write-Host "Mailbox is already being forwarded to $forwardingAddress." -ForegroundColor $successColor
+            return
+        }
     }
     while (-not($foundForwardingMb))
 
@@ -499,14 +582,21 @@ function Prompt-ForwardEmails($upn)
     }
     
     $updatedMailbox = Get-Mailbox -Identity $upn
-    if ($forwardingMailbox.UserPrincipalName -ilike "$($updatedMailbox.ForwardingAddress)*")
+    if ($intendedForwardingName -ieq $updatedMailbox.ForwardingAddress)
     {
         Write-Host "Mailbox was forwarded successfully." -ForegroundColor $successColor
     }
     else
     {        
-        Write-Warning "There was an issue forwarding the mailbox. This will need to be done manually."
+        Write-Warning "There was an issue forwarding the mailbox."
+        Write-Host "Expected forwarding name: $intendedForwardingName" -ForegroundColor $warningColor
+        Write-Host "Actual forwarding name: $($updatedMailbox.ForwardingAddress)" -ForegroundColor $warningColor
     }
+}
+
+function Get-NameFromEmail($email)
+{
+    return ($email.Split('@'))[0]
 }
 
 # main
@@ -521,14 +611,16 @@ Use-Module "ExchangeOnlineManagement"
 TryConnect-AzureAD
 TryConnect-ExchangeOnline
 $user = PromptFor-User
+Show-TimeStamp
 Write-Host "Processing..." -ForegroundColor $infoColor
-SignOut-AllSessions -ObjectId $user.ObjectId
-Block-SignIn -ObjectId $user.ObjectId
-Change-Password -ObjectId $user.ObjectId
+SignOut-AllSessions -AzureUser $user
+Block-SignIn -AzureUser $user
+Set-RandomPassword -AzureUser $user
 HideFrom-GlobalAddressList -UPN $user.UserPrincipalName
 ConvertTo-SharedMailbox -UPN $user.UserPrincipalName
 Remove-AllLicenses -AzureUser $user
-Remove-GroupMembership -UserUpn $user.UserPrincipalName -GroupUpn "corporate@blueravensolar.com"
+Remove-GroupMembership -AzureUser $user -GroupUpn "corporate@blueravensolar.com"
+Remove-AllAdminRoles -AzureUser $user
 Handle-LitigationHold -UPN $user.UserPrincipalName
 Prompt-ForwardEmails -UPN $user.UserPrincipalName
 Read-Host "Press Enter to exit"
